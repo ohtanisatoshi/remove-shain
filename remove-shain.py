ced4e7a404699d431fb8d45c3c90077db3857f55
Satoshi Otani <ohtanisatoshi@gmail.com>
2016-11-25 02:30:54 +0900

    commit

--- remove-shain.py
+++ remove-shain.py
@@ -1,10 +1,11 @@
 from PIL import Image
 
+BLACK_THRESHOLD = 130
 
 def is_red(r, g, b):
     r_g = r - g
     r_b = r - b
-    if r_g > 0 and r_b > 0:
+    if r_g >= 0 and r_b >= 0:
         if r_g > 5 or r_b > 5:
             return True
         else:
@@ -12,7 +13,8 @@ def is_red(r, g, b):
     else:
         return False
 
-img = Image.open("receipt.jpg")
+
+img = Image.open("seikyuusho.jpg")
 img_rgb = img.convert('RGB')
 img_gray = img.convert('L')
 #img_gray.show()
@@ -34,32 +36,100 @@ while(is_changed):
                 p_a_r, p_a_g, p_a_b = img_rgb.getpixel((x, y-1))
                 p_b_r, p_b_g, p_b_b = img_rgb.getpixel((x, y+1))
                 # 1: white, 2: red, 3: black
+                gray_l = 0
                 c_l = 1
                 if is_red(p_l_r, p_l_g, p_l_b):
                     c_l = 2
                 else:
-                    if img_gray.getpixel((x-1, y)) < 150:
+                    gray_l = img_gray.getpixel((x-1, y))
+                    if gray_l < BLACK_THRESHOLD:
                         c_l = 3
+                gray_r = 0
                 c_r = 1
                 if is_red(p_r_r, p_r_g, p_r_b):
                     c_r = 2
                 else:
-                    if img_gray.getpixel((x+1, y)) < 150:
+                    gray_r = img_gray.getpixel((x+1, y))
+                    if gray_r < BLACK_THRESHOLD:
                         c_r = 3
+                gray_a = 0
                 c_a = 1
                 if is_red(p_a_r, p_a_g, p_a_b):
                     c_a = 2
                 else:
-                    if img_gray.getpixel((x, y-1)) < 150:
+                    gray_a = img_gray.getpixel((x, y-1))
+                    if gray_a < BLACK_THRESHOLD:
                         c_a = 3
+                gray_b = 0
                 c_b = 1
                 if is_red(p_b_r, p_b_g, p_b_b):
                     c_b = 2
                 else:
-                    if img_gray.getpixel((x, y+1)) < 150:
+                    gray_b = img_gray.getpixel((x, y+1))
+                    if gray_b < BLACK_THRESHOLD:
                         c_b = 3
                 if c_l == 3 or c_r == 3 or c_a == 3 or c_b == 3:
-                    img_rgb.putpixel((x, y), (0, 0, 0))
+                    r_total = 0
+                    g_total = 0
+                    b_total = 0
+                    gray_p_count = 0
+                    if c_l == 3:
+                        gray_p_count += 1
+                        r_total += p_l_r
+                        g_total += p_l_g
+                        b_total += p_l_b
+                    if c_r == 3:
+                        gray_p_count += 1
+                        r_total += p_r_r
+                        g_total += p_r_g
+                        b_total += p_r_b
+                    if c_a == 3:
+                        gray_p_count += 1
+                        r_total += p_a_r
+                        g_total += p_a_g
+                        b_total += p_a_b
+                    if c_b == 3:
+                        gray_p_count += 1
+                        r_total += p_b_r
+                        g_total += p_b_g
+                        b_total += p_b_b
+
+                    r_new_value = r_total/gray_p_count
+                    g_new_value = g_total/gray_p_count
+                    b_new_value = b_total/gray_p_count
+                    if is_red(r_new_value, g_new_value, b_new_value):
+                        if g_new_value < b_new_value:
+                            r_new_value = g_new_value
+                        else:
+                            r_new_value = b_new_value
+
+                    r_max = 0
+                    g_max = 0
+                    b_max = 0
+                    gray_max = 0
+                    if c_l == 3:
+                        if gray_l < gray_max:
+                            r_max = p_l_r
+                            g_max = p_l_g
+                            b_max = p_l_b
+                    if c_r == 3:
+                        if gray_r < gray_max:
+                            r_max = p_r_r
+                            g_max = p_r_g
+                            b_max = p_r_b
+                    if c_a == 3:
+                        if gray_a < gray_max:
+                            r_max = p_a_r
+                            g_max = p_a_g
+                            b_max = p_a_b
+                    if c_b == 3:
+                        if gray_b < gray_max:
+                            r_max = p_b_r
+                            g_max = p_b_g
+                            b_max = p_b_b
+
+                    #img_rgb.putpixel((x, y), (r_new_value, g_new_value, b_new_value))
+                    img_rgb.putpixel((x, y), (r_max, g_max, b_max))
                     is_changed = True
                 else:
                     if c_l == 2 and c_r == 2 and c_a == 2 and c_b ==2:
